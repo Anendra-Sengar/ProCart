c5ab719c25655e97d55a580bde7ce85439fabe75
anendra sengar <anendrasengar1996@gmail.com>
2024-11-06 13:52:19 +0530

    add css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
-import React from 'react'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Container } from 'react-bootstrap';
 
 const App = () => {
   return (
     <>
-    <Header/>
-    <div>welcome to ProCart </div>
-    <Footer/>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>welcome to ProCart </h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   )
 }
